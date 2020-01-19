f8ccfffeea11a642c5d64383f6e192ef49b5c573
Abhishek Bharti <abhishek054bharti@google.com>
2020-01-19 23:09:56 +0530

    update

---
+++ MergeSort/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class MergeSort
+    {
+        public static void Main()
+        {
+            int[] arr = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100 };
+            Console.WriteLine("Merge Sort");
+            Console.Write("Initial array is: ");
+            Print(arr, arr.Length);
+            MergeSorting(arr, 0, arr.Length - 1);
+            Console.Write("\nSorted Array is:  ");
+            Print(arr, arr.Length);
+        }
+
+        static void MergeSorting(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int mid = start + ((end - start) / 2);
+                MergeSorting(arr, start, mid);
+                MergeSorting(arr, mid + 1, end);
+                Merge(arr, start, mid, end);
+            }
+        }
+
+        static void Merge(int[] arr, int start, int mid, int end)
+        {
+            int i, j, k;
+            int n1 = mid - start + 1;
+            int n2 = end - mid;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            for (i = 0; i < n1; i++)
+            {
+                L[i] = arr[start + i];
+            }
+            for (j = 0; j < n2; j++)
+            {
+                R[j] = arr[mid + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            k = start;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k++] = L[i++];
+                }
+                else
+                {
+                    arr[k++] = R[j++];
+                }
+            }
+            while (i < n1)
+            {
+                arr[k++] = L[i++];
+            }
+            while (j < n2)
+            {
+                arr[k++] = R[j++];
+            }
+        }
+
+        static void Print(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ QuickSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] arr = { 16, 7, 5, 8, 9, 1, 5 };
+            QuickSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("sorted array ");
+            PrintArray(arr);
+        }
+
+        static void QuickSort(int[] arr, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int pIndex = Partition(arr, start, end);
+
+            QuickSort(arr, start, pIndex - 1);
+            QuickSort(arr, pIndex + 1, end);
+        }
+
+        static int Partition(int[] arr, int start, int end)
+        {
+            int pivot = arr[end];
+
+            int pIndex = start;
+
+            for (int j = start; j < end; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    Swap(arr, j, pIndex);
+                    pIndex++;
+                }
+            }
+
+            Swap(arr, pIndex, end);
+
+            return pIndex;
+        }
+
+        static void Swap(int[] arr, int a, int b) // O(1)
+        {
+            if (a == b || arr[a] == arr[b])
+            {
+                return;
+            }
+
+            arr[a] = arr[a] ^ arr[b];
+            arr[b] = arr[a] ^ arr[b];
+            arr[a] = arr[a] ^ arr[b];
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+                Console.Write(arr[i] + " ");
+
+            Console.WriteLine();
+        }
+
+    }
+}
